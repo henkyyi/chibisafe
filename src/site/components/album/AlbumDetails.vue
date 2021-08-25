b3710472f36ea94453eb13dcb4ba9e7b6b69ac68
henkyyi <mc@afsa.se>
2021-08-25 19:38:38 +0200

    Revert "hardcode album url"
    This reverts commit 24c6f046bb4a396a4566dff7b16fe4ca041bd5af.

--- src/site/components/album/AlbumDetails.vue
+++ src/site/components/album/AlbumDetails.vue
@@ -11,7 +11,7 @@
 				label="Link"
 				centered>
 				<a
-					:href="`https://uwu.nu/a/${props.row.identifier}`"
+					:href="`${config.URL}/a/${props.row.identifier}`"
 					target="_blank">
 					{{ props.row.identifier }}
 				</a>
